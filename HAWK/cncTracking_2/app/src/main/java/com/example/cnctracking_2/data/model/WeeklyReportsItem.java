cd492fe37161acd9213b1b5d975c305e83040cbd
muhammad.hamza <muhammad00hamza@gmail.com>
2022-02-09 13:40:47 +0300

    Api model added

---
+++ HAWK/cncTracking_2/app/src/main/java/com/example/cnctracking_2/data/model/WeeklyReportsItem.java
@@ -0,0 +1,50 @@
+package com.example.cnctracking_2.data.model;
+
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+public class WeeklyReportsItem{
+
+	@SerializedName("dataPoints")
+	private List<DataPointsItem> dataPoints;
+
+	@SerializedName("title")
+	private Title title;
+
+	@SerializedName("type")
+	private String type;
+
+	public void setDataPoints(List<DataPointsItem> dataPoints){
+		this.dataPoints = dataPoints;
+	}
+
+	public List<DataPointsItem> getDataPoints(){
+		return dataPoints;
+	}
+
+	public void setTitle(Title title){
+		this.title = title;
+	}
+
+	public Title getTitle(){
+		return title;
+	}
+
+	public void setType(String type){
+		this.type = type;
+	}
+
+	public String getType(){
+		return type;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"WeeklyReportsItem{" + 
+			"dataPoints = '" + dataPoints + '\'' + 
+			",title = '" + title + '\'' + 
+			",type = '" + type + '\'' + 
+			"}";
+		}
+}
